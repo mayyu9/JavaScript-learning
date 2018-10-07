9e5adfcb7a58a0fd61e963782240725fe8ba6180
Thakur Singh <coolmahimsd@gmail.com>
2018-10-07 15:21:55 +0530

    revers an pallindrome

---
+++ pallindrome.js
@@ -0,0 +1,23 @@
+function palindrome(str) {
+
+    var len = str.length;
+    var mid = Math.floor(len/2);
+
+    for ( var i = 0; i < mid; i++ ) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+console.log(palindrome('12321'));
+
+//method 2:
+function pallindrome(str) {
+  if(str === str.split(''.reverse().join('')))
+    return true;
+return false;
+}
---
+++ rever_string.js
@@ -0,0 +1,32 @@
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+console.log(reverseString('hello'));
+
+//mehtod 2
+function rev(str){
+  let str1 = str.split('').reverse().join('');
+  return str1;
+}
+
+
+//method 3:
+
+function rev(str){
+  var str1 = str.split('');
+  var len= str1.length;
+  var tmp;
+  for(let i=0; i< (len)/2; i++){
+    tmp= str1[len-1-i]; // gets elemnts from last
+    str1[len-1-i]=str1[i];
+    str1[i]=tmp;
+  }
+  return str1.join('');
+
+}
+
+console.log(rev('hello'));
