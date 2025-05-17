b07655c1c8ba4d2b25fd1ea0a34b0083fc29ba3c
mayyu9 <coolmahimsd@gmail.com>
2025-05-17 12:38:47 +0530

    cunt duplicates

--- Array_elements_frequency.js
+++ Array_elements_frequency.js
@@ -6,6 +6,7 @@ this logic is getting the count but duplicate values still exist.
 
 
 var a = ['mango', 'apple', 'kiwi', 'mango', 'apple',  'mango', 'apple', 'mango'];
+
 function fun(a){
  let res = [];
  debugger;
@@ -25,4 +26,22 @@ var b = a.map( (data,i) => {
 return res;
 }
 
+// above logic tc: 0(n^2)
 console.log(fun(a));
+
+
+function countDuplicates(a) {
+  let output = {};
+
+  a.map(d => {
+    output[d] = (output[d] || 0) + 1;
+  });
+
+  return output;
+}
+
+console.log(countDuplicates(a)) 
+
+// op of above pblrm:
+
+// { mango: 4, apple: 3, kiwi: 1 }
